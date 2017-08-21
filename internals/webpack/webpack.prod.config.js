95f79fa90c1058ca79bb50ce7c33b0579674d724
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-08-21 20:05:51 -0300

    feature(files): add internals

---
+++ internals/webpack/webpack.prod.config.js
@@ -0,0 +1,117 @@
+const webpack = require('webpack');
+const yargs = require('yargs');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const options = yargs
+  .alias('p', 'optimize-minimize')
+  .alias('d', 'debug')
+  .option('port', {
+    default: '8080',
+    type: 'string'
+  })
+  .argv;
+
+const baseConfig = {
+  entry: './client/js/main.js',
+  output: {
+    path: './public',
+    publicPath: '/public/',
+    filename: 'js/scripts.min.js'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue'
+    },
+    extensions: ['.js', '.vue', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['vue', 'es2015', 'stage-0']
+          }
+        }
+      },
+      {
+        test: /\.vue/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'vue-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: true,
+                localIdentName: '[name]_[local]_[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                config: {
+                  path: path.resolve(__dirname, './config/postcss.config.js')
+                }
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }),
+        include: path.resolve(__dirname, '../../source/')
+      },
+      {
+        test: /\.(jpe?g|jpg|gif|ico|png|woff|woff2|eot|ttf)$/,
+        include: path.resolve(__dirname, '../../source/'),
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production'
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      comments: false,
+      sourceMap: false
+    }),
+    new ExtractTextPlugin(`[name].min.css`, {
+      allowChunks: true
+    })
+  ],
+  devtool: '#eval-source-map',
+};
+
+module.exports =  baseConfig;
